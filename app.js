1c8c1738659c6448a74c4d70724372dff1205bb9
Elue-dev <eluewisdom@gmail.com>
2021-08-19 22:49:35 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const identity = 'Elue Dubem Wisdom';
+
+const intro = document.querySelector('.intro');
+
+intro.appendChild(document.createTextNode(`Hello there, I am ${identity}, a Frontend Developer`));
